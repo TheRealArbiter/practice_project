09867356f4ba014e841c46fd147c027b1e2eff2b
Luis Guzman <luisguzman.dev@gmail.com>
2021-08-14 22:13:46 -0500

    se elimina ruta innecesaria

--- server.js
+++ server.js
@@ -9,7 +9,4 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'jsx');
 app.engine('jsx', require('express-react-views').createEngine());
 
-app.get('/', require('./routes').index)
-
 app.get('/', require('./routes').index);
-
