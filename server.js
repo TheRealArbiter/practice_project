48edb1f1a1b65615ed7e1d7c2c479e7e1d241185
Luis Guzman <luisguzman.dev@gmail.com>
2021-08-14 22:06:50 -0500

    initial commit

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+const server = app.listen(8000, 'localhost', function(err) {
+    if(err) return console.log(err)
+    console.log('Listening on port 8000')
+})
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jsx');
+app.engine('jsx', require('express-react-views').createEngine());
+
+app.get('/', require('./routes').index)
+
+app.get('/', require('./routes').index);
+
